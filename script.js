d7df1a4f4bb7e4f0076ef67284596f326fc20655
NotEnoughBacon <jboyk3@hotmail.com>
2023-01-13 13:41:27 -0700

    added comments to JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// We get the hour ids for each of the hours so we can log their data index for storage
 var timeRows = [ 
   $('#hour-08'),
   $('#hour-09'),
@@ -11,6 +12,7 @@ var timeRows = [
   $('#hour-17')
 ]
 
+// These are the ids for text area in each of the hours so we can store the text data later
 var textArea = [
   $('#textArea1'),
   $('#textArea2'),
@@ -24,11 +26,14 @@ var textArea = [
   $('#textArea10')
 ]
 
+// An empty array so that we can store the text content in order of index
 var textAreaText = ['', '', '', '', '', '', '', '', '', '']
 
 var time;
 var currentHour;
 
+// This will pull the filled array from local storage and set the text content on screen to whatever was saved.
+// If there is nothing saved it wont to anything to prevent an error on load
 function getFromLocal() {
 
   var retrieveData = JSON.parse(localStorage.getItem('savedData'))
@@ -43,6 +48,7 @@ function getFromLocal() {
   }
 }
 
+// This saves the text content accoding to the data index value so that it is in order to where the user saved it at
 function saveToLocal(index) {
 
   textAreaText[index] = textArea[index].val()
@@ -51,6 +57,8 @@ function saveToLocal(index) {
   localStorage.setItem('savedData', stringData)
 }
 
+// This is the clock that both gets the full date to display at the top as well at the hour for resetting the classes in reference to the current time
+// This will update every second to show a more accurate time at the top, as well as runs the change color function
 function updateClock() {
   time = dayjs().format('MM/DD/YYYY HH:mm:ss');
   currentHour = dayjs().format('HH')
@@ -59,8 +67,8 @@ function updateClock() {
   setTimeout(updateClock, 1000)
 }
 
-
-
+// This takes in the current hour from the clock and depending on the row (I already have it set up to 24 hour time to make it easier)
+// and removes all classes so that we can add the correct class
 function changeColor (currentHour) {
 
   for (var i = 0; i < 10; i++) {
@@ -85,13 +93,14 @@ function changeColor (currentHour) {
   }
 }
 
+// When you click a save button it will grab the data index of that row, so that we can save it to local storage later
 $('button').on('click', function(){
   var index = $(this).data('index')-1
   saveToLocal(index)
   console.log(index)
 })
 
-
+// These are the starting functions for the clock, updating the color based on the time, and pulling any data that may be saved in local storage
 updateClock();
 changeColor();
 getFromLocal();
